5366ea93dfc183c82e34a61b00d1802c58cb1809
SecLord <balkrishnaoli98@gmail.com>
2022-05-25 06:28:45 -0400

    Updated JS that blure the Image

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const image = document.querySelector('.image')
 
 let load = 0
 
-let int = setInterval(blurring, 1)
+let int = setInterval(blurring, 100)
 
 function blurring() {
-  setInterval(load++,0.005) 
+  load++
 
   if (load > 99) {
     clearInterval(int)
   }
 
   loadText.innerText = `${load}%`
-  
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  image.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
